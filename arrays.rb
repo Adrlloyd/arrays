42548be300ef194e03696f5715aaee9865ee9fe5
Alastair Lloyd <dex8056@aol.com>
2022-09-14 09:22:09 -0600

    Add methods

--- arrays.rb
+++ arrays.rb
@@ -1,7 +1,15 @@
 strings = ["a", "b", "c", "d"]
+#pop removes the last element
+strings.pop
 
 intergers = [1, 2, 3, 4]
+#push adds on to the end
+intergers.push(5)
 
 floats = [10.1, 11.1, 12.1, 13.1]
+#shift removes the first element
+floats.shift
 
 boolean = [1 > 2, 2 > 3, 3 > 4, 4 < 5]
+#unshift adds to the front
+boolean.shift(0 > 1)
